d337b662c16e9b515d9ee10e64c8b618849b541a
campbell <campbellrehu@me.com>
2017-07-05 14:25:56 +1200

    Added Pushover connection capability

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const tessel = require('tessel-io');
 const five = require('johnny-five');
+const push = require('pushover-notifications');
 const board = new five.Board({
   io: new tessel()
+});
+
+const keys = require('./keys');
+const USER_KEY = keys.USER_KEY;
+const TOKEN = keys.TOKEN;
+
+const p = new push({
+  user: USER_KEY,
+  token: TOKEN
 });
 
 board.on('ready', () => {
@@ -11,7 +21,20 @@ board.on('ready', () => {
     invert: true
   });
 
-  door.on('open', () => led.on());
-  door.on('close', () => led.off());
+  door.on('open', () => {
+    led.on();
+    const msg = {
+      message: 'The circuit has been broken!'
+    };
+    p.send(msg, (err, result) => {
+      print = (err) ? err : result;
+      console.log(print);
+    });
+  });
+
+  door.on('close', () => {
+    led.off();
+    console.log('The circuit is now closed');
+  });
 });
 
